e6ade5d3adcca8dd70cf73633c77b3b445fe377d
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-09-13 15:34:21 -0400

    Add items function

--- shoppingList.js
+++ shoppingList.js
@@ -3,3 +3,23 @@ var shoppingList = [["yogurt", 1], ["apples", 6], ["bacon", 1], ["bagels", 6], [
 console.log(shoppingList);
 
 console.log("\n========\n");
+
+function addCrucialItems(item1, item2) {
+
+}
+
+function addGeneralItems(get1, get2) {
+  shoppingList.push(get1, get2);
+  console.log(`${get1} and ${get2} added to shopping list! \n  Updated List: ${shoppingList}`)
+}
+
+function checkOff(item) {
+  for (var i = 0, 1 < shoppingList.length; i++) {
+    if (shoppingList[i] === item) {
+      //remove item from list
+      console.log(`${item} removed from shopping list. Updated list: ${shoppingList}`)
+    }
+  }
+
+
+}
