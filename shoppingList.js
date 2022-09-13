34351d2497457411dd8c253952bbf6116530bbbb
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-09-13 17:44:13 -0400

    Add remove items function

--- shoppingList.js
+++ shoppingList.js
@@ -4,22 +4,38 @@ console.log(shoppingList);
 
 console.log("\n========\n");
 
-function addCrucialItems(item1, item2) {
 
-}
 
 function addGeneralItems(get1, get2) {
   shoppingList.push(get1, get2);
-  console.log(`${get1} and ${get2} added to shopping list! \n  Updated List: ${shoppingList}`)
+  console.log(`${get1} and ${get2} added to shopping list! \n  Updated List: ${shoppingList}`);
 }
 
-function checkOff(item) {
-  for (var i = 0, 1 < shoppingList.length; i++) {
-    if (shoppingList[i] === item) {
-      //remove item from list
-      console.log(`${item} removed from shopping list. Updated list: ${shoppingList}`)
-    }
-  }
+addGeneralItems(["granola bars, 10"], ["chicken, 1"]);
 
 
+
+function addCrucialItems(item1, item2) {
+  shoppingList.unshift(item1, item2);
+  console.log(`Oh no! You're out of ${item1} and ${item2}! Updates shopping list: ${shoppingList}`);
 }
+
+addCrucialItems(["toilet paper", 12], ["butter", 1]);
+
+
+
+var shoppingList2 = ["apples", "pears", "bananas"]
+console.log(shoppingList2);
+
+// function checkOff(item) {
+// }
+var index = shoppingList2.indexOf("apples");
+console.log(index);
+if (index > -1) {
+    console.log(shoppingList2.splice(index, 1));
+}
+
+  console.log(`Item removed from shopping list. Updated list: ${shoppingList2}`);
+
+
+// checkOff("apples");
